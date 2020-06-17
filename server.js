718658947d1883c31168f28281978e2f0f5a3683
kanishkodz <kanishk@ourdesignz.in>
2020-06-17 17:21:36 +0530

    Working on error handling in script

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const csvReader = require('fast-csv');
 const path = require('path');
-
-const loginUrl = 'https://login.i.ezproxy.nypl.org/login?qurl=http://library.morningstar.com%2fMembership%2fEndSession.html';
-const fundsUrl = 'http://library.morningstar.com.i.ezproxy.nypl.org/Selectors/Fund/Selector.html';
-const username = 'hankmoody789';
-const password = '1111';
+const dotenv = require('dotenv');
+dotenv.config();
 
 /**
  * Read CSV file
@@ -35,23 +32,23 @@ const read_file = async () =>{
 		const fileData = await read_file();
 		let data = [];
 
-		await page.goto(loginUrl);
-		await page.type('[name="user"]', username);
-		await page.type('[name="pass"]', password);
+		await page.goto(process.env.LOGIN_URL);
+		await page.type('[name="user"]', process.env.USERNAME);
+		await page.type('[name="pass"]', process.env.PASSWORD);
 		
 		const form = await page.$('#access-databases');
 		await form.evaluate(form => form.submit());
 		await page.waitForSelector('a[href^="/Selectors/Fund/Selector.html"]');
-		await page.goto(fundsUrl);
+		await page.goto(process.env.FUNDS_URL);
 		// await page.waitForNavigation();
 		await page.waitForSelector('input#AutoCompleteBox');
-
 		if (fileData.length) {
 			console.log(fileData.length);
-			// let row = fileData[0];
+			// let row = fileData[37];
+			// console.log(row);
 			for(let i = 0; i < Object.keys(fileData).length; i++) {
-			// for (const row of fileData) {
 				let row = fileData[i];
+
 				// await page.waitForNavigation();
 				await page.evaluate(() => document.querySelector('input#AutoCompleteBox').click()); 
 
@@ -59,14 +56,56 @@ const read_file = async () =>{
 					document.querySelector('input#AutoCompleteBox').value = ''
 				});
 				await page.type('input#AutoCompleteBox', row.Symbol);
-				await page.waitFor(2000);
+				await page.waitFor(4000);
+
+				/* await page.waitForSelector('div#AutoCompleteDropDown');
+				const display = await page.evaluate(() => {
+					let dropDownElement = document.getElementById('AutoCompleteDropDown');
+					return dropDownElement.css;
+					// return dropDownElement.style.display;
+				});
+				console.log("display : ", display); */
+
+				let dropDownElement = await page.$('table.ACDropDownStyle');
+				if (!dropDownElement) {
+					console.log(row.Symbol , " - dropDownElement TRY 1 : ", dropDownElement);
+					await page.waitFor(4000);
+					dropDownElement = await page.$('table.ACDropDownStyle');
+					if (!dropDownElement) {
+						console.log(row.Symbol , " - dropDownElement TRY 2 : ", dropDownElement);
+						// continue;
+					}
+				}
+				// await page.waitForSelector('table.ACDropDownStyle');
+
 				await page.keyboard.press("Enter");
 				await page.waitForNavigation();
-				await page.waitForSelector('span.security-info-name');
+				await page.waitFor(2000);
+
+				let nameElement = await page.$('span.security-info-name');
+				if (!nameElement) {
+					console.log(row.Symbol , " - nameElement TRY 1 : ", nameElement);
+					await page.waitFor(4000);
+					nameElement = await page.$('span.security-info-name');
+					if (!nameElement) {
+						console.log(row.Symbol , " - nameElement TRY 2 : ", nameElement);
+						continue;
+					}
+				}
+
+				// await page.waitForSelector('span.security-info-name');
+				await page.waitForSelector('span.chart-iframe-full-chart-label');
+				await page.waitFor(4000);
+
+				/* const stockName = await page.evaluate(() => {
+					let stockElement = document.querySelector('span.security-info-name');
+					return stockElement.textContent;
+				});
+				console.log("stockName : ", stockName); */
 
 				const nameHandle = await page.$('span.security-info-name');
 				const stockName = await page.evaluate(span => span.innerHTML, nameHandle);
-				console.log(stockName)
+				// console.log("stockName : ", stockName);
 				await page.waitForSelector('span.legend-label');
 
 				let legends = await page.evaluate(() => {
@@ -92,15 +131,16 @@ const read_file = async () =>{
 				});
 				// console.log(stockName);
 				let rowData = {
-					userId: username,
+					userId: process.env.USERNAME,
 					stockSymbol: row.Symbol,
 					stockName,
 					fairValue
 				};
 				console.log(rowData);
 				data.push(rowData);
-				await page.waitFor(1000);
+				// await page.waitFor(1000);
 			}
+
 			// console.log(data);
 			// console.log(data.length);
 			
